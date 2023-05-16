e3c437f558e754208f73919c2b14a3ce7c7f1fe7
Diego Rios <riosdiego9418@gmail.com>
2023-05-16 16:28:19 -0300

    feature/cadastrosys

--- Main.py
+++ Main.py
@@ -21,5 +21,37 @@ class Pessoa:
   # fazer um sistema para cadastrar animal e pessoa
 
 
+class SistemaAdocao:
+    def __init__(self):
+        self.animais = []
+        self.pessoas = []
 
+    def cadastro_animal(self, tipo, idade, cor, porte, particularidade):
+        animal = Animal(tipo, idade, cor, porte, particularidade)
+        self.animais.append(animal)
+        print('Animal cadastrado com sucesso!')
 
+    def cadastro_pessoa(self, nome, contato, especie_interesse, preferencia):
+        pessoa = Pessoa(nome, contato, especie_interesse, preferencia)
+        self.pessoas.append(pessoa)
+        print('Pessoa cadastrada com sucesso!')
+
+    def buscar_animais_por_caracteristicas(self, especie_interesse, preferencia):
+        animais_encontrados = []
+        for animal in self.animais:
+            if animal.tipo == especie_interesse and animal.cor == preferencia:
+                animais_encontrados.append(animal)
+        return animais_encontrados
+
+    def gerar_relatorio(self):
+        relatorio = "Relatório de cruzamento de espécies disponíveis x possíveis candidatos:\n"
+        for pessoa in self.pessoas:
+            animais_encontrados = self.buscar_animais_por_caracteristicas(pessoa.especie_interesse, pessoa.preferencia)
+            relatorio += f"Nome: {pessoa.nome}\nContato: {pessoa.contato}\nAnimais encontrados:\n"
+            if animais_encontrados:
+                for animal in animais_encontrados:
+                    relatorio += f"Tipo: {animal.tipo}, Cor: {animal.cor}, Porte: {animal.porte}\n"
+            else:
+                relatorio += "Nenhum animal encontrado\n"
+            relatorio += "\n"
+        return relatorio
