ee78a02d2ce4154c117eeae543ebd73dbad7eebf
Diego Rios <riosdiego9418@gmail.com>
2023-05-16 16:26:55 -0300

    Initial

---
+++ Main.py
@@ -0,0 +1,25 @@
+class Animal:
+    def __init__(self, tipo, idade, cor, porte, particularidade):
+        self.tipo = tipo
+        self.idade = idade
+        self.cor = cor
+        self.porte = porte
+        self.particularidade = particularidade
+
+
+class Pessoa:
+    def __init__(self, nome, idade, tipo_animal, idade_animal, cor_animal, porte_animal, particularidade_animal):
+        self.nome = nome
+        self.idade = idade
+        self.tipo_animal = tipo_animal
+        self.idade_animal = idade_animal
+        self.cor_animal = cor_animal
+        self.porte_animal = porte_animal
+        self.particularidade_animal = particularidade_animal
+
+
+  # fazer um sistema para cadastrar animal e pessoa
+
+
+
+
