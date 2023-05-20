7deeeecfa8545cde74a9c7c00fc39f84e01db7ca
Lucass_Santana <127268469+LucasSantanaa@users.noreply.github.com>
2023-05-20 20:34:16 -0300

    Update Main.py

--- Main.py
+++ Main.py
@@ -8,8 +8,9 @@ class Animal:
 
 
 class Pessoa:
-    def __init__(self, nome, idade, tipo_animal, idade_animal, cor_animal, porte_animal, particularidade_animal):
+    def __init__(self, nome,contato, idade, tipo_animal, idade_animal, cor_animal, porte_animal, particularidade_animal):
         self.nome = nome
+        self.contato = contato
         self.idade = idade
         self.tipo_animal = tipo_animal
         self.idade_animal = idade_animal
@@ -19,6 +20,24 @@ class Pessoa:
 
 
   # fazer um sistema para cadastrar animal e pessoa
+   # Esse def e o de para inserir novo tipo de animal, pode olhar  e ver se ta tudo certo dps eu finalizo essa parte e mando aq. dps so arrumamos o codigo na segunda.
+def inserir_tipo_animal(dicionario_animais):
+    tipo = input("Digite o novo tipo de animal: ")
+    dicionario_animais[tipo] = []
+
+def cadastrar_animal(dicionario_animais):
+    tipo = input("Digite o tipo do animal: ")
+    if tipo in dicionario_animais:
+        idade = input("Digite a idade do animal: ")
+        cor = input("Digite a cor do animal: ")
+        porte = input("Digite o porte do animal: ")
+        particularidade = input("Digite alguma particularidade do animal: ")
+
+        animal = Animal(tipo, idade, cor, porte, particularidade)
+        dicionario_animais[tipo].append(animal)
+        print("Animal cadastrado com sucesso!")
+    else:
+        print("Tipo de animal não existe.")
 
 
 class SistemaAdocao:
