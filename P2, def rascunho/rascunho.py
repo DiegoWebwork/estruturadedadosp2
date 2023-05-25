a337c0cd4ab3674f131a69ae48b2aa8cd89268f8
Lucass_Santana <127268469+LucasSantanaa@users.noreply.github.com>
2023-05-25 10:13:06 -0300

    Update rascunho.py

--- P2, def rascunho/rascunho.py
+++ P2, def rascunho/rascunho.py
@@ -1,3 +1,8 @@
+#a importação de sys é usada para chamar a função sys.exit(1) no caso de uma entrada inválida do usuário, 
+#interrompendo a execução do programa com um código de saída 1. Isso é uma maneira de lidar com erros e terminar o programa de forma controlada.
+import sys
+
+# Classe para representar um animal
 class Animal:
     def __init__(self, tipo, idade, cor, porte, particularidade):
         self.tipo = tipo
@@ -5,57 +10,16 @@ class Animal:
         self.cor = cor
         self.porte = porte
         self.particularidade = particularidade
-        
-class Pessoa:
-    def __init__(self, nome, contato, especie_interesse, preferencia_animal):
+
+# Classe para representar uma pessoa interessada em adotar
+class Interessado:
+    def __init__(self, nome, contato, especie, preferencia):
         self.nome = nome
         self.contato = contato
-        self.especie_interesse = especie_interesse
-        self.preferencia_animal = preferencia_animal
-        
-class Fila:
-    def __init__(self):
-        self.items = []
-
-    def enfileirar(self, item):
-        self.items.append(item)
-
-    def desenfileirar(self):
-        if not self.esta_vazia():
-            return self.items.pop(0)
-
-    def esta_vazia(self):
-        return len(self.items) == 0
-
-class Pilha:
-    def __init__(self):
-        self.items = []
-
-    def empilhar(self, item):
-        self.items.append(item)
-
-    def desempilhar(self):
-        if not self.esta_vazia():
-            return self.items.pop()
-
-    def esta_vazia(self):
-        return len(self.items) == 0    
-    
-#dps so arrumamos o codigo.
-def inserir_tipo_animal(dicionario_animais):
-    tipo = input("Digite o novo tipo de animal: ")
-    dicionario_animais[tipo] = []
-
-def cadastrar_animal(dicionario_animais):
-    tipo = input("Digite o tipo do animal: ")
-    if tipo in dicionario_animais:
-        idade = input("Digite a idade do animal: ")
-        cor = input("Digite a cor do animal: ")
-        porte = input("Digite o porte do animal: ")
-        particularidade = input("Digite alguma particularidade do animal: ")
-
-        animal = Animal(tipo, idade, cor, porte, particularidade)
-        dicionario_animais[tipo].append(animal)
-        print("Animal cadastrado com sucesso!")
-    else:
+        self.especie = especie
+        self.preferencia = preferencia
         print("Tipo de animal não existe.")
+
+        
+        
+        
